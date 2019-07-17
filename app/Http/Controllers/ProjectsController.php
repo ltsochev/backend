20a797ccf8b96d9554b7ba80dd5a836dffbb803a
Lachezar Tsochev <xtremescript@gmail.com>
2019-07-17 17:00:21 +0300

    blank

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use function App\Support\seo;
+use App\Models\ProjectRequest;
 
 class ProjectsController extends Controller
 {
@@ -33,7 +35,7 @@ class ProjectsController extends Controller
         // budget-type, phone, start-month, start-year, start-date
         // launch-month, launch-year, launch-date
 
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name'  => 'required',
             'email' => 'required|email',
             'project-type' => 'required',
@@ -41,7 +43,45 @@ class ProjectsController extends Controller
             'needs' => 'required',
         ]);
 
-        dd($validatedData);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // If we reach this, we can safely submit the data
+        $startDate = $launchDate = null;
+
+        if ($request->get('start-year') > 0 &&
+            $request->get('start-month') > 0 && $request->get('start-date', 0) == 0) {
+                $startDate = Carbon::parse(
+                    sprintf('%s-%s-01 00:00:00', $request->get('start-year'), $request->get('start-month'))
+                );
+        }
+
+        if ($request->get('launch-year') > 0 &&
+            $request->get('launch-month') > 0 &&
+            $request->get('launch-date', 0) == 0) {
+                $launchDate = Carbon::parse(
+                    sprintf('%s-%s-01 00:00:00', $request->get('launch-year'), $request->get('launch-month'))
+                );
+        }
+
+        $projectRequest = new ProjectRequest([
+            'name'  => $request->get('name'),
+            'email' => $request->get('email', 'johndoe@domain.com'),
+            'phone' => $request->get('phone'),
+            'description' => $request->get('description'),
+            'type' => $request->get('project-type'),
+            'project_needs' => json_encode($request->get('needs')),
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->server('User-Agent'),
+            'start_date' => $startDate,
+            'launch_date' => $launchDate,
+            'budget' => $request->get('budget-type', 0)
+        ]);
+
+        $projectRequest->save();
+
+        dd($projectRequest);
     }
 
     public function getProject($projectSlug)
--- app/Models/ProjectRequest.php
+++ app/Models/ProjectRequest.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ProjectRequests extends Model
+class ProjectRequest extends Model
 {
     protected $table = 'project_requests';
     protected $fillable = [
